70372f1b0fc3cee38b5b2747965e4842a3346c7e
ftrimble <forest@blendlabs.com>
2019-03-13 13:30:59 -0700

    Dont include dist in package'

--- dist/src/invert.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Inverts the given promise - i.e. throws an error if it completes successfully and returns the
- * error if it throws one.
- *
- * @param {Promise} promise - the promise to invert
- * @param {string} message - the message to throw if the promise resolves
- * @returns the error thrown by the promise
- */
-export declare function invert<T>(promise: Promise<T>, message?: string): Promise<any>;
