c109213443bbf0da57e964e6a2aea96974df1fd4
ayman <ayman@blend.com>
2019-05-16 14:31:44 -0700

    Correct SettleAll comment

--- src/settleAll.ts
+++ src/settleAll.ts
@@ -6,8 +6,8 @@ export interface SettledPromises<T, V> {
 /**
  * Attempts to settle all promises in promises in parallel, calling errFn when a promise rejects.
  * Similar to Promise.all, but does not fail fast. For resolved promises, the result array contains
- * the resolved value at the same index as the promise. For rejected promises, the result array
- * contains the return value of errFn at the same index as the promise.
+ * resolved values in the same order as the promises. For rejected promises, the result array
+ * contains the return values of errFn in the same order as the promises.
  *
  * @param {Promise<T>[]} promises - An array of promises to attempt to settle.
  * @param {Function} errFn - The function to call when a promise rejects.
