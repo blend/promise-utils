82a62955596e9794b4b1ff7b2a8ba323c9038e7f
Eugene Marinelli <eugene@blend.com>
2019-06-20 06:52:55 -0700

    Remove tslint check from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,6 @@ module.exports = function(grunt) {
   const TS_SRC_FILES = ['src/**/*.ts'];
   const TS_TEST_FILES = ['test/**/*.ts'];
   const ALL_TS_FILES = [...TS_SRC_FILES, ...TS_TEST_FILES];
-  const COMPILED_SRC_FILES = ['dist/**/*.js'];
-  const COMPILED_TEST_FILES = ['dist/test/**/*.test.js'];
   const ALL_FILES = [...ALL_TS_FILES, 'Gruntfile.js', 'package-lock.json', 'package.json', 'tsconfig.json', 'tslint.*'];
 
   // Project configuration.
@@ -25,33 +23,12 @@ module.exports = function(grunt) {
     run: {
       test: {
         cmd: 'npm',
-        args: ['run', 'testCode']
+        args: ['run', 'test']
       },
       compile: {
         cmd: 'npm',
         args: ['run', 'prepare']
       }
-    },
-
-    tslint: {
-      test: {
-        options: {
-          configuration: 'tslint.test.js',
-          fix: true
-        },
-        files: {
-          src: TS_TEST_FILES,
-        },
-      },
-      src: {
-        options: {
-          configuration: 'tslint.js',
-          fix: true
-        },
-        files: {
-          src: TS_SRC_FILES,
-        },
-      },
     },
 
     watch: {
@@ -66,12 +43,10 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-ts');
-  grunt.loadNpmTasks('grunt-tslint');
   grunt.loadNpmTasks('grunt-prettier');
   grunt.loadNpmTasks('grunt-force-task');
   grunt.loadNpmTasks('grunt-run');
 
-  grunt.registerTask('test', [ 'run:compile', 'run:test', 'prettier', 'tslint:src', 'tslint:test']);
+  grunt.registerTask('test', ['run:test']);
   grunt.registerTask('default', ['force:test', 'watch']);
 };
