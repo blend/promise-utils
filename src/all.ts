48167d06e820a3be770c248bc462f1a19bf781e8
Forest Trimble <forest@blend.com>
2021-05-06 17:00:48 -0700

    commments

--- src/all.ts
+++ src/all.ts
@@ -6,6 +6,10 @@ import { settleAll } from './settleAll';
  * is unsafe when multiple promises reject. This will handle failures appropriately,
  * though it will defer handling until the last promise has resolved/rejected.
  *
+ * In the case of a rejection, the thrown error will be the rejection of the first
+ * promise in the array (as opposed to the first promise to be rejected temporally),
+ * with all other errors attached in the `otherErrors` property.
+ *
  * @param {Promise<T>[]} promises - An array of promises to attempt to resolve.
  * @returns A list of resolved values of promises.
  */
@@ -16,12 +20,8 @@ export async function all<T>(promises: readonly Promise<T>[]): Promise<T[]> {
     if (intermediateResults.errors.length > 1 && primaryError instanceof Error) {
       primaryError.message = `${primaryError.message}... and ${intermediateResults.errors.length -
         1} other errors`;
-      const otherErrors = [];
-      for (let i = 1; i < intermediateResults.errors.length; ++i) {
-        otherErrors.push(intermediateResults.errors[i]);
-      }
       // tslint:disable-next-line:no-any (intentionally augmenting error)
-      (primaryError as any).otherErrors = otherErrors;
+      (primaryError as any).otherErrors = intermediateResults.errors.slice(1);
     }
     throw primaryError;
   }
