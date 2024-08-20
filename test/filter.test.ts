8df1b54de90aca535699b4aff6f004d59748a9a1
Tushar Verma <tushar@blend.com>
2024-08-20 16:37:39 -0400

    fix: lower test delays

--- test/filter.test.ts
+++ test/filter.test.ts
@@ -16,7 +16,7 @@ test('returns empty array when given no input', async t => {
 test('filters arrays and maintains order', async t => {
   const input = [1, 2, 3, 4, 5, 6, 7, 8];
   const output = await promiseUtils.filter(input, async (value: any) => {
-    await delay(_.random(10000)); // Add a variable delay to ensure we don't maintain order by luck
+    await delay(_.random(5000)); // Add a variable delay to ensure we don't maintain order by luck
     return value > 3;
   });
   t.deepEqual(output, [4, 5, 6, 7, 8]);
@@ -25,7 +25,7 @@ test('filters arrays and maintains order', async t => {
 test('filters arrays with indices and maintains order', async t => {
   const input = [1, 2, 3, 4, 5, 6, 7, 8];
   const output = await promiseUtils.filter(input, async (value: any, i: number) => {
-    await delay(_.random(10000)); // Add a variable delay to ensure we don't maintain order by luck
+    await delay(_.random(5000)); // Add a variable delay to ensure we don't maintain order by luck
     return i % 2 === 0;
   });
   t.deepEqual(output, [1, 3, 5, 7]);
